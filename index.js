76570ef7eabc7a9fbc00a9cf796a7a47a157a8f3
MikeRafael <mikerafaelpl10@hotmail.com>
2022-09-25 20:45:10 -0300

    atualização2.0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const { Server } = require('socket.io');
 const io = new Server(server);
-const port = 3000;
+port = process.env.PORT || 80
 
 app.use(express.static(path.join(__dirname, 'public')))
 
